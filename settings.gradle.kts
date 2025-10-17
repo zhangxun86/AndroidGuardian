6c51d3d13e21a5949bd2e33298a25ed4e060ad01
zx <zhangxun86@163.com>
2025-10-17 10:01:43 +0800

    add file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
             url = uri("https://maven.pkg.github.com/zhangxun86/my-public-aar-libs")
             // 加上凭证，以私有方式访问 (Kotlin DSL 语法)
             credentials {
-                username = providers.gradleProperty("gpr.user").orNull
-                password = providers.gradleProperty("gpr.key").orNull
+                username = "zhangxun86"
+                password = "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"
             }
         }
 
